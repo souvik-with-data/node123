d3390c3666941e345abc108b7ddc4d6aef00c939
souvik-with-data <souvikshee2004@gmail.com>
2025-06-20 18:33:00 +0530

    Add files via upload

---
+++ ass4_1.js
@@ -0,0 +1,18 @@
+// create a node js project using express.
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
