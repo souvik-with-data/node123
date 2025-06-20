76be6079f046f119ac4dc9f0c9e93848f98e3280
souvik-with-data <souvikshee2004@gmail.com>
2025-06-20 18:24:28 +0530

    Add files via upload

---
+++ assignment1.5.js
@@ -0,0 +1,22 @@
+//what is repl in node js and how it is setful for testing javascript code real time.
+> 67+23
+90
+> for(let i=0;i<3;i++)
+... console.log(i);
+0
+1
+2
+undefined
+> function hellow(){
+... console.log("hellow");
+... }
+undefined
+> hellow()
+hellow
+undefined
+> var a="souvik"
+undefined
+> a
+'souvik'
+> const name=(myname)=>{
+... console.log("my name is ${myname});}
