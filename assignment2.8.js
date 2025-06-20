8cd377342fc4b02d32efedb5d5222f7687a73800
souvik-with-data <souvikshee2004@gmail.com>
2025-06-20 18:27:53 +0530

    Add files via upload

---
+++ assignment2.8.js
@@ -0,0 +1,51 @@
+//create a node js project with package .json file and describe each with an external package.
+package name: (souvik)
+version: (1.0.0)
+git repository:
+license: (ISC)
+About to write to C:\Users\S\Desktop\nodejs\package.json:
+
+{
+  "name": "souvik",
+  "version": "1.0.0",
+  "description": "pakage.json",
+  "main": "index.js",
+  "scripts": {
+    "test": "node index.js"
+  },
+  "keywords": [
+    "npm",
+    "node",
+    "package"
+  ],
+  "author": "souvik",
+  "license": "ISC",
+  "dependencies": {
+    "color": "^5.0.0",
+    "dotenv": "^16.4.7"
+  },
+  "devDependencies": {}
+}
+
+//output
+
+{
+  "name": "souvik",
+  "version": "1.0.0",
+  "description": "pakage.json",
+  "main": "index.js",
+  "scripts": {
+    "test": "node index.js"
+  },
+  "keywords": [
+    "npm",
+    "node",
+    "package"
+  ],
+  "author": "souvik",
+  "license": "ISC",
+  "dependencies": {
+    "color": "^5.0.0",
+    "dotenv": "^16.4.7"
+  }
+}
